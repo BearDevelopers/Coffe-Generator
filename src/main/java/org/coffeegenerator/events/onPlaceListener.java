246ab2df11e27404451f70d94e8527d0cd7c1d26
BearDeveloper <dr.polar099@gmail.com>
2023-04-27 09:41:45 -0300

    opa

--- src/main/java/org/coffeegenerator/events/onPlaceListener.java
+++ src/main/java/org/coffeegenerator/events/onPlaceListener.java
@@ -1,37 +1,37 @@
 package org.coffeegenerator.events;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitScheduler;
 import org.coffeegenerator.Coffee_Generator;
 
 public class onPlaceListener implements Listener {
-    public BukkitScheduler scheduler;
+
     FileConfiguration config = Coffee_Generator.getInstance().getConfig();
+
+    @EventHandler
     public void onPlace(BlockPlaceEvent e) {
         Player p = e.getPlayer();
         Block blocks = e.getBlockPlaced();
         Location loc = blocks.getLocation();
-        if (p.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase(config.getString("generator.name"))) {
-            loc.getBlock().setType(Material.DIAMOND);
+        if (e.getBlockPlaced().getType() == Material.STONE) {
+            loc.getBlock().setType(Material.DIAMOND_BLOCK);
             double y = blocks.getY() + 2;
+            p.sendMessage(ChatColor.RED + "Bloco colocado!, Gerando itens...");
             Location locs = new Location(blocks.getWorld(), blocks.getX(), y, blocks.getZ());
-            scheduler.runTaskTimer(Coffee_Generator.getInstance(), new Runnable() {
-                @Override
-                public void run() {
-                    for (int i = 0; i > config.getInt("generator.itens-drops"); i++) {
-                        loc.getWorld().dropItemNaturally(locs, new ItemStack(Material.DIAMOND_ORE));
-                    }
+            Bukkit.getScheduler().runTaskTimer(Coffee_Generator.getInstance(), () -> {
+                for (int i = 0; i < config.getInt("generator.itens-drops"); i++) {
+                    loc.getWorld().dropItemNaturally(locs, new ItemStack(Material.DIAMOND_ORE));
                 }
             }, 0L, 20L * config.getInt("generator.segunds-of-spawn"));
-            scheduler = Bukkit.getScheduler();
             loc.getWorld().dropItem(locs, new ItemStack(Material.DIAMOND_ORE));
         }
     }
